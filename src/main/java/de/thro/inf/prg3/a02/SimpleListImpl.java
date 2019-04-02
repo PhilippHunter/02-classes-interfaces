58c8782eb935bd514a97826504cd9e3f9388a0e2
PhilippHunter <philippjaeger98@web.de>
2019-04-02 11:08:44 +0200

    assignment02 linked list

--- src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
@@ -1,11 +1,79 @@
 package de.thro.inf.prg3.a02;
 
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList, Iterable {
 
-	// TODO: Implement the required methods.
+  private Element head;
+  private int size;
 
+  public SimpleListImpl() {
+    head = null;
+  }
+
+  // TODO: Implement the required methods.
+  @Override
+  public void add(Object item) {
+    size++;
+
+    if (head == null) {
+      head = new Element(item);
+      return;
+    }
+
+    Element current = this.head;
+    while (current.next != null) {
+      current = current.next;
+    }
+    current.next = new Element(item);
+  }
+
+  /*Interface implementations*/
+  @Override
+  public int size() {
+    return size;
+  }
+
+  @Override
+  public SimpleList filter(SimpleFilter filter) {
+    SimpleListImpl sl = new SimpleListImpl();
+    for (Object o : this) {
+      if (filter.include(o)) sl.add(o);
+    }
+    return sl;
+  }
+
+  @Override
+  public Iterator iterator() {
+    return new SimpleIteratorImpl();
+  }
+
+  class SimpleIteratorImpl implements Iterator {
+    private Element current = head;
+
+    @Override
+    public boolean hasNext() {
+      return current != null;
+    }
+
+    @Override
+    public Object next() {
+      Object tmp = current.item;
+      current = current.next;
+      return tmp;
+    }
+  }
+
+  private static class Element {
+    private Object item;
+    private Element next;
+
+    public Element(Object item) {
+      this.item = item;
+    }
+  }
 }
